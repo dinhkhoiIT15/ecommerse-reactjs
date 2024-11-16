7353cd8267395592622689566314a984f6abac2f
dinhkhoiIT15 <dinhkh1514@gmail.com>
2024-11-17 00:53:16 +0700

    Create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import MyFooter from "@components/Footer/Footer";
 import MyHeader from "@components/Header/Header";
 import MainLayout from "@components/Layout/Layout";
-import MyButton from "./components/Button/Button";
 
 function App() {
     return (
         <>
             <MainLayout>
-                {/* <MyHeader />
-                Contents
-                <MyFooter /> */}
-                <MyButton />
+                <MyHeader />
             </MainLayout>
         </>
     );

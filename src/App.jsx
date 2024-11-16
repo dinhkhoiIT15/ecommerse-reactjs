22f22ce543e2a41b1cf8784575631d82b650309d
dinhkhoiIT15 <dinhkh1514@gmail.com>
2024-11-16 21:14:09 +0700

    config alias vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Hello from "@components/Hello/Hello";
 
 function App() {
-    return <div>Ecommerse</div>;
+    return (
+        <div>
+            <Hello /> Ecommerse
+        </div>
+    );
 }
 
 export default App;

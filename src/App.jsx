62827a774e3fccb1c512acc01728f45e601d23ce
dinhkhoiIT15 <dinhkh1514@gmail.com>
2024-11-18 00:55:59 +0700

    create Section Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Homepage from "@components/HomePage/HomePage";
+import Info from "@components/Info/Info";
 
 function App() {
     return (
         <>
             <Homepage />
+            <Info />
         </>
     );
 }

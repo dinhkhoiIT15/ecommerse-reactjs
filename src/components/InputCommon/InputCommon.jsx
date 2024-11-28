2c09b5c9fc231c2739de110b3164768beb4964c6
dinhkhoiIT15 <dinhkh1514@gmail.com>
2024-11-28 21:59:44 +0700

    validate form sign in

--- src/components/InputCommon/InputCommon.jsx
+++ src/components/InputCommon/InputCommon.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import styles from "./styles.module.scss";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import { ErrorMessage } from "formik";
 
-function InputCommon({ label, type, isRequired = false }) {
-    const { container, labelInput, boxInput, boxIcon } = styles;
-
+function InputCommon({ label, type, isRequired = false, ...props }) {
+    const { container, labelInput, boxInput, boxIcon, errorMsg } = styles;
+    const { formik, id } = props;
     const [showPassword, setShowPassword] = useState(false);
 
     const isPassword = type === "password";
@@ -15,18 +16,28 @@ function InputCommon({ label, type, isRequired = false }) {
         setShowPassword(!showPassword);
     };
 
+    const isErr = formik.touched[id] && formik.errors[id];
+    const messErr = formik.errors[id];
+
     return (
         <div className={container}>
             <div className={labelInput}>
                 {label} {isRequired && <span>*</span>}
             </div>
             <div className={boxInput}>
-                <input type={isShowTextPassword} />
+                <input
+                    type={isShowTextPassword}
+                    {...props}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values[id]}
+                />
                 {isPassword && (
                     <div className={boxIcon} onClick={handleShowPassword}>
                         {showPassword ? <FiEyeOff /> : <FiEye />}
                     </div>
                 )}
+                {isErr && <div className={errorMsg}>{messErr}</div>}
             </div>
         </div>
     );

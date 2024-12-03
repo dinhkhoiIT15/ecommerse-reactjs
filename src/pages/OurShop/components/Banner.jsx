97930c288078dc744b197dbfb6b9043b095d137d
dinhkhoiIT15 <dinhkh1514@gmail.com>
2024-12-03 19:12:41 +0700

    complete banner

--- src/pages/OurShop/components/Banner.jsx
+++ src/pages/OurShop/components/Banner.jsx
@@ -1,11 +1,24 @@
+import CountdownTimer from "@components/CountdownTimer/CountdownTimer";
 import styles from "../styles.module.scss";
+import Button from "@components/Button/Button";
 
 function Banner() {
-    const { containerBanner } = styles;
+    const { containerBanner, contentBox, title, boxBtn, countdownBox } = styles;
+    const targetDate = "2025-12-31T00:00:00";
 
     return (
         <>
-            <div className={containerBanner}>Banner</div>
+            <div className={containerBanner}>
+                <div className={contentBox}>
+                    <div className={countdownBox}>
+                        <CountdownTimer targetDate={targetDate} />
+                    </div>
+                    <div className={title}>The Classics Make A Comeback</div>
+                    <div className={boxBtn}>
+                        <Button content={"Buy now"} />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
